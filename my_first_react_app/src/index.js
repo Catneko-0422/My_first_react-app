169e27e65f0acaa36cf353f024396a02c5cc8dad
nekocat <linyian0422@gmail.com>
2024-11-02 01:09:22 +0800

    add react project

---
+++ my_first_react_app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Home from './pages/Home'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
+
